8c0a0fd0b5c0c0aa05e95a8b4198f68da8a09c98
anmapie <anna@apiechowski.com>
2018-12-30 22:27:13 -0800

    Ensure Instagram images are loaded on page load if section is in view

--- js/home.js
+++ js/home.js
@@ -4,35 +4,45 @@ $(document).ready(function() {
   var imageSection = $("#play");
   var imageGrid = $(".instagram-grid");
   
-  // load instagram images when section is first scrolled into view
+  // only load instagram images if section is in view
+  var loadImages = function() {
+    imagesLoaded = true;
+
+    $.ajax({
+      type: "GET",
+      dataType: "json",
+      url: "https://us-central1-sanguine-link-226918.cloudfunctions.net/recent-instagram-posts",
+      success: function(response) {
+        var posts = response["posts"].slice(0, 12);
+
+        imageGrid.show();
+        
+        $.each(posts, function(_index, post) {     
+          var imageHtml = "<img src='" + post["image"] + "'></img>"
+          var imageLink = "<div class='image-container'><a href='" + post["url"] + "'>" + imageHtml + "</a></div>";
+          imageGrid.append($(imageLink));
+        });
+      },
+      error: function(response) {
+        var iframe = "<iframe width='560' height='315' src='https://www.youtube.com/embed/0IagRZBvLtw' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>";
+        imageSection.children(".section-content-wrapper").append($(iframe));
+      }
+    });
+  }
+
+  // check if the instagram section is already visible on load
+  if (imageSection.offset().top <= $(window).height()) {
+    loadImages();
+  }
+
+  // otherwise, load images when instagram section is scrolled into view
   $(window).scroll(function() {
     if (imagesLoaded) { return; }
 
-    var scrollTriggerHeight = imageSection.offset().top + imageSection.outerHeight() - $(window).height() -400;
+    var scrollTriggerHeight = imageSection.offset().top + imageSection.outerHeight() - $(window).height() - 400;
   
     if ($(this).scrollTop() > scrollTriggerHeight) {
-      imagesLoaded = true;
-
-      $.ajax({
-        type: "GET",
-        dataType: "json",
-        url: "https://us-central1-sanguine-link-226918.cloudfunctions.net/recent-instagram-posts",
-        success: function(response) {
-          var posts = response["posts"].slice(0, 12);
-
-          imageGrid.show();
-          
-          $.each(posts, function(_index, post) {     
-            var imageHtml = "<img src='" + post["image"] + "'></img>"
-            var imageLink = "<div class='image-container'><a href='" + post["url"] + "'>" + imageHtml + "</a></div>";
-            imageGrid.append($(imageLink));
-          });
-        },
-        error: function(response) {
-          var iframe = "<iframe width='560' height='315' src='https://www.youtube.com/embed/0IagRZBvLtw' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>";
-          imageSection.children(".section-content-wrapper").append($(iframe));
-        }
-      });
+      loadImages();
     }
   });
 
